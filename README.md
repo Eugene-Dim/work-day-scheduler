e4dd01d6cb28a1a2605747b3445c333b54bf8fc5
Eugene Dimov <eugenedostoevsky@Eugenes-MBP.MG8702>
2023-02-25 17:05:09 -0800

    .

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# work-day-scheduler
+# Work Day Scheduler Starter Code
+The goal of this project is to create a scheduler for the current day.
+
+Description:
+The final project displays typical a work day schedule form 7am - 5pm. 
+The user is able to enter to do lists or other information and save it to the scheduler. 
+The information is saved so that once the page refreshes the information stays on the page. 
+The clear button will clear everything on the page. 
+
---
+++ script.js
@@ -0,0 +1,47 @@
+var currentTime = moment();
+let now = moment().hour();
+
+
+displayCurrentDate(currentTime);
+
+// display current date
+function displayCurrentDate() {
+    document.getElementById('currentDay').textContent = currentTime.format('dddd, MMMM Do YYYY');
+};
+
+// Color Timeblocks
+$('.time-block').each(function(){
+    var tbHour = parseInt($(this).attr('id'));
+    if (now > tbHour){
+        $(this).addClass('past')
+    } else if (now < tbHour){
+        $(this).addClass('future')
+    } else if (now === tbHour){
+        $(this).addClass('present')
+    }
+});
+
+// local storage
+$('.saveBtn').on('click', function(){
+    // e.preventDefault();
+    var timeId = $(this).parent().attr('id');
+    var userInput = $(this).siblings('textarea').val();
+
+    localStorage.setItem(timeId, userInput);
+    
+});
+
+$('textarea').each(function(){
+    var storedEvents = $(this).parent().attr('id');
+    var savedEvents = localStorage.getItem(storedEvents);
+    if (savedEvents !== null){
+        $(this).val(savedEvents);
+    }
+});
+
+// clear button
+$('#clearBtn').on('click', function(){
+    var clearEvent = '  '
+    localStorage.clear()
+    $('textarea').val(clearEvent);
+})
